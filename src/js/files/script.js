28ca0d5a24727de72673a5169acb8528c4eda6de
sergei00026 <sergei57fx@mail.ru>
2022-08-20 23:58:46 +0500

    feat: blocks

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,30 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+const decorationPageVideo = document.querySelector('.decoration-page__video');
+const decorationPageButton = document.querySelector('.decoration-page__button');
+
+function playVideo() {
+	if (decorationPageVideo.pause) {
+		decorationPageVideo.play()
+		decorationPageButton.style.display = 'none';
+	} else {
+		decorationPageVideo.pause()
+	}
+}
+
+decorationPageButton.addEventListener("click", playVideo);
+
+
+function pauseVideo() {
+	if (decorationPageVideo.play) {
+		decorationPageVideo.pause();
+		decorationPageButton.style.display = 'block';
+
+	} else {
+		decorationPageVideo.play()
+	}
+}
+
+decorationPageVideo.addEventListener("click", pauseVideo);
